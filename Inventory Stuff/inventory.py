11bccc8ee503b700f9858e6d1262202c20ba488d
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-05-28 12:31:33 +0300

    Update inventory.py

--- Inventory Stuff/inventory.py
+++ Inventory Stuff/inventory.py
@@ -2,3 +2,32 @@
 def product_list():
     products = Product.query.all()
     return render_template('product_list.html', products=products)
+
+@app.route('/place_order', methods=['POST'])
+def place_order():
+    
+
+    order = Order(customer_name=customer_name, customer_email=customer_email, product_id=product_id)
+
+    product = Product.query.get(product_id)
+    product.inventory -= 1
+
+    db.session.add(order)
+    db.session.commit()
+
+    # ...
+
+@app.route('/place_order', methods=['POST'])
+def place_order():
+    
+    product = Product.query.get(product_id)
+    if product.inventory <= 0:
+        return "Product is out of stock"
+
+    product.inventory -= 1
+
+    db.session.add(order)
+    db.session.commit()
+
+    # ...
+    
