ed8f5a09644f0a526d370acfbda5e85a70958736
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-05-28 12:39:46 +0300

    Create discounts_db.py

---
+++ Discounts_Promotions Stuff/discounts_db.py
@@ -0,0 +1,4 @@
+class Discount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20), unique=True, nullable=False)
+    discount_percent = db.Column(db.Float, nullable=False)
